7c7235ab82f730cdc4f1e5f22f19871e383f64e1
Simon <simon.casteran@epitech.eu>
2022-06-30 14:16:17 +0200

    feat(tdd_api): removes unused variable in editswagger, removed tag admin from swagger.temp.yaml

--- back/src/config/editSwaggerYaml.js
+++ back/src/config/editSwaggerYaml.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 function editSwaggerYaml(){
-    const fileUri = './swagger.temp.yaml'
     let content = fs.readFileSync('./swagger.temp.yaml', 'utf8');
     content = content.replace('${PORT}', process.env.PORT)
     fs.writeFileSync('./swagger.yaml', content);
