553c7f6d15071dd971de47d96df88ac7d4a790ed
Léo Levacher <leo.levacher@sogeti.com>
2022-07-01 13:46:50 +0200

    feat: update header

--- front/frontend_react/src/tests/index/index.test.tsx
+++ front/frontend_react/src/tests/index/index.test.tsx
@@ -3,18 +3,21 @@ import Home from '../../pages/Home';
 import '@testing-library/jest-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+const renderHome = () => {
+	render(
+		<Router>
+			<Home />
+		</Router>
+	);
+};
+
 describe('Home', () => {
-	it('renders a heading', () => {
-		render(
-			<Router>
-				<Home />
-			</Router>
-		);
+	beforeEach(renderHome);
 
-		const heading = screen.getByRole('heading', {
-			name: /Web project/i,
+	it('renders a search input and when not empty display a popover', () => {
+		const searchInput = screen.getByRole('textbox', {
+			name: /search/i,
 		});
-
-		expect(heading).toBeInTheDocument();
+		expect(searchInput).toBeInTheDocument();
 	});
 });
